edd6ac5573569a433f211671f6dcf25672b42a2d
Iamneo <techadmin@iamneo.ai>
2023-08-24 14:35:46 +0000

    Backup Commit

--- jspservlet-app-bmi-calculator/src/main/java/controller/MainController.java
+++ jspservlet-app-bmi-calculator/src/main/java/controller/MainController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns= {"/jspservelt-app-bmi-calculator/main"})
+@WebServlet(urlPatterns= {"/jspservlet-app-bmi-calculator/main"})
 public class MainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
